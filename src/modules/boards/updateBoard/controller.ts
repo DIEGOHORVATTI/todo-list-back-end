69e97c7958976938fe6a335ded5128330e151f46
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-09-08 22:14:17 -0300

    Refactor getAllBoardsController and getAllBoardsService to use 'boards' instead of 'users' for consistency and clarity.

--- src/modules/boards/updateBoard/controller.ts
+++ src/modules/boards/updateBoard/controller.ts
@@ -3,10 +3,10 @@ import { updateBoardService } from './service'
 import type { RequestHandler } from 'express'
 
 export const updateBoardController: RequestHandler = async (req, res) => {
-  const user = await updateBoardService({
+  const board = await updateBoardService({
     id: req.params?.id,
     ...req.body
   })
 
-  return res.status(201).json({ message: 'User updated', user })
+  return res.status(201).json(board)
 }
